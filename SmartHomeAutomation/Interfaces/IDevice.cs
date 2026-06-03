356f5b1f4eb3d2f944f763fd4b56eb8b176f1ab0
tapan0810 <arceus0810@gmail.com>
2026-06-03 11:41:05 +0530

    feat: Implemented Interfaces for SmartHomeAutomation Application

---
+++ SmartHomeAutomation/Interfaces/IDevice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeAutomation.Interfaces
+{
+    internal interface IDevice
+    {
+        String Name { get; set; }
+        bool IsOn { get; set; }
+
+        void TurnOn();
+        void TurnOff();
+    }
+}
---
+++ SmartHomeAutomation/Interfaces/IObserver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeAutomation.Interfaces
+{
+    internal interface IObserver
+    {
+        void Update(int value);
+    }
+}
