879e813df19fd215bb0880710190d853378afe7e
tapan0810 <arceus0810@gmail.com>
2026-06-03 12:21:45 +0530

    feat : Implemented Room model for all the devices

---
+++ SmartHomeAutomation/Models/Room.cs
@@ -0,0 +1,35 @@
+﻿using SmartHomeAutomation.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeAutomation.Models
+{
+    public class Room
+    {
+        public string Name { get; set; }
+        public List<Device> Devices { get; set; }
+        public Room(string name)
+        {
+            Name = name;
+            Devices = new List<Device>();
+        }
+
+        public void AddDevice(Device device)
+        {
+            Devices.Add(device);
+        }
+
+        public void ShowDevices()
+        {
+            Console.WriteLine($"\nRoom : {Name}");
+
+            foreach(var device in Devices)
+            {
+                Console.WriteLine(device.Name);
+            }
+        }
+    }
+}
