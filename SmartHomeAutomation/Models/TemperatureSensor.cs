387931ea9151a5befb652dee1b1ddf36d8e8fa38
tapan0810 <arceus0810@gmail.com>
2026-06-03 13:11:51 +0530

    feat : Implemented tempratureSensor

---
+++ SmartHomeAutomation/Models/TemperatureSensor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SmartHomeAutomation.Delegates;
+
+namespace SmartHomeAutomation.Models
+{
+    public class TemperatureSensor
+    {
+        private int _temprature;
+
+        public event TempratureChangedHandler? TempratureChanged;
+
+        public void SetTemprature(int temprature)
+        {
+            _temprature = temprature;
+
+            Console.WriteLine($"Temperature Sensor: Current Temprature = {_temprature}°C");
+
+            TempratureChanged?.Invoke(temprature);
+        }
+
+        public int GetTemprature()
+        {
+            return _temprature;
+        }
+
+    }
+}
