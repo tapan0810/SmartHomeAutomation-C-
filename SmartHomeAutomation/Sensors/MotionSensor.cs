e46b578a5e2fa2526105f57f3a4c457b1b953e49
tapan0810 <arceus0810@gmail.com>
2026-06-03 13:13:36 +0530

    feat : Implemented MotionSensor

---
+++ SmartHomeAutomation/Sensors/MotionSensor.cs
@@ -0,0 +1,20 @@
+﻿using SmartHomeAutomation.Delegates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeAutomation.Sensors
+{
+    public class MotionSensor
+    {
+        public event MotionDetectedHandler? MotionDetected;
+
+        public void DetectMotion(string location)
+        {
+            Console.WriteLine($"Motion Detected at {location}!");
+            MotionDetected?.Invoke(location);
+        }
+    }
+}
